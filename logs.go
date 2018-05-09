ef02f5c7c2ae5a26ed73825cc594ca6ed061ddc5
Nilesh Simaria <nsimaria@juniper.net>
2018-05-08 18:38:53 -0700

    better logging

--- logs.go
+++ logs.go
@@ -1,59 +1,39 @@
 package main
 
 import (
-	"fmt"
+	"io"
 	"log"
 	"os"
 )
 
-/*
-	Log it, each go routine collecting JTI data has its own log file but user can
-	specify common log file to log for all devices so we need protection.
-	We also need protection for printing on terminal. Caller can control it using
-	boolean safe argument.
-*/
 func l(safe bool, jctx *JCtx, s string) {
-	if *print {
-		switch safe {
-		case true:
-			gmutex.Lock()
-			fmt.Printf(s)
-			gmutex.Unlock()
-		case false:
-			fmt.Printf(s)
-		}
-	} else if jctx.config.Log.Logger != nil {
-		switch safe {
-		case true:
-			gmutex.Lock()
-			jctx.config.Log.Logger.Printf(s)
-			gmutex.Unlock()
-
-		case false:
-			jctx.config.Log.Logger.Printf(s)
-		}
-	}
+	jctx.config.Log.Logger.Printf(s)
 }
 
 func logInit(jctx *JCtx) {
 	file := jctx.config.Log.File
-	if file != "" {
-		if *print {
-			fmt.Println("Both print and log options are specified, ignoring log")
-		} else {
-			f, err := os.OpenFile(file, os.O_CREATE|os.O_RDWR|os.O_TRUNC, 0666)
-			if err != nil {
-				fmt.Printf("Could not create log file(%s): %v\n", file, err)
-			} else {
-				flags := 0
-				if !jctx.config.Log.CSVStats {
-					flags = log.LstdFlags
-				}
-				jctx.config.Log.Logger = log.New(f, "", flags)
-				jctx.config.Log.FileHandle = f
-				fmt.Printf("logging in %s for %s:%d [periodic stats every %d seconds]\n",
-					jctx.config.Log.File, jctx.config.Host, jctx.config.Port, jctx.config.Log.PeriodicStats)
-			}
+	var out io.Writer
+
+	if *print {
+		out = os.Stdout
+		if file != "" {
+			log.Println("Both print and log options are used, ignoring log")
+		}
+	} else if file != "" {
+		var err error
+		out, err = os.OpenFile(file, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0666)
+		if err != nil {
+			log.Printf("Could not create log file(%s): %v\n", file, err)
 		}
 	}
+
+	flags := 0
+	if !jctx.config.Log.CSVStats {
+		flags = log.LstdFlags
+	}
+
+	jctx.config.Log.Logger = log.New(out, "", flags)
+
+	log.Printf("logging in %s for %s:%d [periodic stats every %d seconds]\n",
+		jctx.config.Log.File, jctx.config.Host, jctx.config.Port, jctx.config.Log.PeriodicStats)
 }

266551f9773e9bd23cc133d542e4e36833273f37
Nilesh Simaria <nileshsimaria@gmail.com>
2018-05-28 09:18:29 -0700

    remove half baked gnmi support

--- gnmi/gencode.sh
+++
@@ -1,3 +0,0 @@
-#cd to $GOPATH/src/github.com/nileshsimaria/jtimon/gnmi
-
-protoc -I . --go_out=plugins=grpc,Mgoogle/protobuf/descriptor.proto=github.com/golang/protobuf/protoc-gen-go/descriptor:. gnmi.proto 
